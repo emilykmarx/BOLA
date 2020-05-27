1e878fbf61e73765c844fdfbfece7cc57f5ab5ee
Emily Marx <emilykmarx@gmail.com>
2020-05-27 15:24:34 -0700

    add script to parse output of size/ssim Influx queries

---
+++ parse_avg_size_ssim.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 27 14:26:21 2020
+
+@author: emily
+"""
+from collections import defaultdict
+import numpy as np
+
+# For size and SSIM:
+# Influx query groups by {channel, format}; average over channel to get a single ladder of len 10
+
+def parse(filename):   
+    with open(filename) as f:
+        # channel => [ 10 sizes ]
+        ladders = defaultdict(lambda: []) 
+        cur_channel = ""
+        # Get ladder from each channel
+        for line in f:
+            fields = line.split()
+            if len(fields) == 0:
+                continue
+            if fields[0] == "tags:":
+                # Channel line
+                cur_channel = fields[1]
+            elif fields[0][0] == "1":
+                # Size line
+                val = float(fields[1])
+                ladders[cur_channel].append(val)
+        # Sort ladders increasing
+        for channel,ladder in ladders.items():
+            ladder.sort()
+       
+        # Average ladders across channels    
+        return np.mean(ladders.values(), axis=0)
+    
+print('Avg size (bytes):')
+print(parse('avg_sizes'))    
+
+print('Avg ssim_index:')
+print(parse('avg_ssims'))    
